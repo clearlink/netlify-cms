fd1e376f70a596f45fee66fcdaa8628137f3df86
Jimmy Cleveland <Jimmy.Cleveland@clearlink.com>
2018-08-03 15:31:02 -0600

    Add watch for enter keypress on input fields

--- packages/netlify-cms-widget-components/src/ComponentsControl.js
+++ packages/netlify-cms-widget-components/src/ComponentsControl.js
@@ -1,72 +1,117 @@
-import React, { Component } from 'react'
-import { SortableContainer, SortableElement, SortableHandle, arrayMove } from 'react-sortable-hoc'
-import { css } from 'react-emotion'
+import React, { Component } from 'react';
+import {
+  SortableContainer,
+  SortableElement,
+  SortableHandle,
+  arrayMove,
+} from 'react-sortable-hoc';
+import styled, { css } from 'react-emotion';
 
-const DragHandle = SortableHandle(() => <span>:: </span>)
-const ComponentPart = SortableElement(({value}) => (
-  <div className={
-    css`
+const StyledDragHandle = styled('span')`
+  margin-right: 10px;
+  cursor: pointer;
+`;
+
+const StyledContent = styled('div')`
+  flex: 1;
+`;
+
+const DragHandle = SortableHandle(() => (
+  <StyledDragHandle>::</StyledDragHandle>
+));
+
+class Contenty extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+  }
+
+  handleKeyPress(evt) {
+    if (evt.key === 'Enter') {
+      console.log("Bruce Willy AND Liam Neesus")
+    }
+  }
+
+  render() {
+    return <StyledContent onKeyPress={this.handleKeyPress} contentEditable>{this.props.value}</StyledContent>
+  }
+}
+
+const ComponentPart = SortableElement(({ value }) => (
+  <div
+    className={css`
       display: flex;
       width: 100%;
-      border: 1px dashed lightgray;
-      padding: .5rem;
-      margin: .5rem 0;
+      padding: 0.5rem;
+      margin: 0.5rem 0;
+      border: 1px dashed transparent;
+
+      &:hover {
+        border-color: lightgray;
+      }
+      &:active {
+        border-color: orange;
+      }
     `}
   >
     <DragHandle />
-    <div contentEditable>{value}</div>
+    <Contenty value={value} />
   </div>
-  )
-)
+));
 
-const ComponentsWrapper = SortableContainer(({items}) => (
+const ComponentsWrapper = SortableContainer(({ items }) => (
   <div>
-    {items.map((value, idx) => <ComponentPart key={idx} index={idx} value={value} />)}
+    {items.map((value, idx) => (
+      <ComponentPart key={idx} index={idx} value={value} />
+    ))}
   </div>
-  )
-)
-
+));
 
 export default class ComponentsControl extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       content: '',
       items: ['1', '2', '3', '4'],
-    }
+    };
 
-    this.handleInput = this.handleInput.bind(this)
+    this.handleInput = this.handleInput.bind(this);
   }
 
   handleInput(evt) {
-    console.log(evt.target)
+    console.log(evt.target);
     this.setState({
       content: evt.target.textContent,
-    })
+    });
   }
 
   handleSortEnd = ({ oldIndex, newIndex }) => {
     this.setState({
       items: arrayMove(this.state.items, oldIndex, newIndex),
-    })
-  }
+    });
+  };
 
   render() {
-    const { field } = this.props
-    const cats = field.get('categories')
+    const { field } = this.props;
+    const cats = field.get('categories');
 
     for (const cat of cats) {
-      console.log(cat.get('label'))
-      console.log(cat.get('name'))
-      console.log(cat.get('components'))
+      console.log(cat.get('label'));
+      console.log(cat.get('name'));
+      console.log(cat.get('components'));
     }
 
     return (
       <div className="components__container">
-        <ComponentsWrapper items={this.state.items} onSortEnd={this.handleSortEnd} useDragHandle={true} />
+        <ComponentsWrapper
+          items={this.state.items}
+          onSortEnd={this.handleSortEnd}
+          useDragHandle={true}
+        />
         <textarea value={this.state.content} />
       </div>
-    )
+    );
   }
 }
