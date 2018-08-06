7e0bbc56fded860ffa05c341b101d75a42a33569
Aaron Hill <68696c6c@gmail.com>
2018-08-06 16:27:32 -0600

    Made the enter key create a new sortable block.

--- packages/netlify-cms-widget-components/src/ComponentsControl.js
+++ packages/netlify-cms-widget-components/src/ComponentsControl.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
   SortableContainer,
   SortableElement,
@@ -6,6 +7,7 @@ import {
   arrayMove,
 } from 'react-sortable-hoc';
 import styled, { css } from 'react-emotion';
+import { Icon } from 'netlify-cms-ui-default'
 
 const StyledDragHandle = styled('span')`
   margin-right: 10px;
@@ -17,56 +19,79 @@ const StyledContent = styled('div')`
 `;
 
 const DragHandle = SortableHandle(() => (
-  <StyledDragHandle>::</StyledDragHandle>
+  <StyledDragHandle>
+    <Icon type="drag-handle" />
+  </StyledDragHandle>
 ));
 
-class Contenty extends Component {
+class ContentBlock extends Component {
   constructor(props) {
     super(props)
-
     this.handleKeyPress = this.handleKeyPress.bind(this)
   }
 
   handleKeyPress(evt) {
     if (evt.key === 'Enter') {
-      console.log("Bruce Willy AND Liam Neesus")
+      evt.preventDefault()
+      this.props.handleEnter(this.props.position)
     }
   }
 
   render() {
-    return <StyledContent onKeyPress={this.handleKeyPress} contentEditable>{this.props.value}</StyledContent>
+    return (
+      <StyledContent
+        id={`block-${this.props.position}`}
+        onKeyPress={this.handleKeyPress}
+        dangerouslySetInnerHTML={{__html: this.props.value}}
+        contentEditable
+      />
+    )
   }
 }
 
-const ComponentPart = SortableElement(({ value }) => (
-  <div
-    className={css`
-      display: flex;
-      width: 100%;
-      padding: 0.5rem;
-      margin: 0.5rem 0;
-      border: 1px dashed transparent;
+ContentBlock.propTypes = {
+  handleEnter: PropTypes.func.isRequired,
+  position: PropTypes.number.isRequired,
+};
 
-      &:hover {
-        border-color: lightgray;
-      }
-      &:active {
-        border-color: orange;
-      }
-    `}
-  >
-    <DragHandle />
-    <Contenty value={value} />
-  </div>
-));
+const ComponentPart = SortableElement(({ value, handleEnter, position }) => {
+  const style = css`
+    display: flex;
+    width: 100%;
+    padding: 0.5rem;
+    margin: 0.5rem 0;
+    border: 1px dashed transparent;
+    &:hover {
+      border-color: lightgray;
+    }
+    &:active {
+      border-color: orange;
+    }
+  `
+  return (
+    <div className={style}>
+      <DragHandle />
+      <ContentBlock value={value} handleEnter={handleEnter} position={position} />
+    </div>
+  )
+});
 
-const ComponentsWrapper = SortableContainer(({ items }) => (
+ComponentPart.propTypes = {
+  handleEnter: PropTypes.func.isRequired,
+  position: PropTypes.number.isRequired,
+};
+
+const ComponentsWrapper = SortableContainer(({ items, handleEnter }) => (
   <div>
     {items.map((value, idx) => (
-      <ComponentPart key={idx} index={idx} value={value} />
+      <ComponentPart key={idx} value={value} handleEnter={handleEnter} position={idx}/>
     ))}
   </div>
 ));
+
+ComponentsWrapper.propTypes = {
+  handleEnter: PropTypes.func.isRequired,
+};
 
 export default class ComponentsControl extends Component {
   constructor(props) {
@@ -74,24 +99,35 @@ export default class ComponentsControl extends Component {
 
     this.state = {
       content: '',
-      items: ['1', '2', '3', '4'],
+      items: ['1', '2', '3', '4',],
     };
 
     this.handleInput = this.handleInput.bind(this);
+    this.addContentBlock = this.addContentBlock.bind(this);
   }
 
   handleInput(evt) {
-    console.log(evt.target);
     this.setState({
       content: evt.target.textContent,
     });
   }
 
+  // @TODO: drag-n-drop sorting isn't working.
   handleSortEnd = ({ oldIndex, newIndex }) => {
     this.setState({
       items: arrayMove(this.state.items, oldIndex, newIndex),
     });
   };
+
+  addContentBlock(index) {
+    const items = [...this.state.items];
+    const newIndex = index + 1
+    items.splice(newIndex, 0, '');
+    this.setState({ items }, () => {
+      // @TODO: there's got to be a React Sortable way of selecting newly added elements...
+      document.getElementById(`block-${newIndex}`).focus()
+    });
+  }
 
   render() {
     const { field } = this.props;
@@ -109,6 +145,7 @@ export default class ComponentsControl extends Component {
           items={this.state.items}
           onSortEnd={this.handleSortEnd}
           useDragHandle={true}
+          handleEnter={this.addContentBlock}
         />
         <textarea value={this.state.content} />
       </div>
