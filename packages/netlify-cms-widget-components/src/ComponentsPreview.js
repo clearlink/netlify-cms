5bbc2847fd9f61be1036324a0ea8e49c62128345
Tyler Ipson <tyler.ipson@clearlink.com>
2018-08-24 14:49:50 -0600

    Sets previewContainer to one instance

--- packages/netlify-cms-widget-components/src/ComponentsPreview.js
+++ packages/netlify-cms-widget-components/src/ComponentsPreview.js
@@ -3,20 +3,17 @@ import PropTypes from 'prop-types';
 import { WidgetPreviewContainer } from 'netlify-cms-ui-default';
 import { markdownToHtml } from '../../netlify-cms-widget-markdown/src/serializers';
 
-const logMessage = (label, ...messages) => {
-  console.log(`%c COMPONENTS PREVIEW ${label}`, 'color: lime', ...messages);
-};
-
 const ComponentsPreview = (props) => {
-  logMessage('props', props);
   if (props.value === null) {
     return null;
   }
-  return props.value.map((item, index) => {
-    logMessage('value map', item);
-    const html = markdownToHtml(item.value);
-    return <WidgetPreviewContainer dangerouslySetInnerHTML={{ __html: html }}/>;
-  });
+
+  // Creates new array of markdown values then joins on double return
+  const html = props.value.map(item => item.value).join('\n\n');
+
+  return (
+    <WidgetPreviewContainer dangerouslySetInnerHTML={{ __html: markdownToHtml(html) }} />
+  )
 };
 
 ComponentsPreview.propTypes = {
