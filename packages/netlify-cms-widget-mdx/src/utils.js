ee3bf75c7fba9bb522c300093193505b89741c17
Aaron Hill <68696c6c@gmail.com>
2018-08-30 12:41:33 -0600

    Added MDX Node classes and tests.

--- packages/netlify-cms-widget-mdx/src/utils.js
+++ packages/netlify-cms-widget-mdx/src/utils.js
@@ -1,2 +1,89 @@
-export const TYPE_CONTENT = 'jimmy';
-export const TYPE_COMPONENT = 'tyler';
+import uuid from 'uuid/v4';
+import validate from 'uuid-validate';
+
+export class BaseNode {
+  constructor(value, id = '') {
+
+    // ID validation.
+    if (id === '' || !validate(id, 4)) {
+      this.id = uuid();
+    } else {
+      this.id = id;
+    }
+
+    // Value validation.
+    if (typeof value === 'undefined') {
+      throw 'value is required';
+    } else {
+      this.value = value;
+    }
+  }
+}
+
+export const MARKDOWN_TYPES = {
+  text: {
+    symbol: null,
+    pattern: null,
+  },
+  listUnordered: {
+    symbol: '* ',
+    pattern: /^\* ./,
+  },
+  listOrdered: {
+    symbol: '* ',
+    pattern: /^\d\. ./,
+  },
+};
+
+const isValidMarkdownType = (type) => {
+  for (const t in MARKDOWN_TYPES) {
+    if (MARKDOWN_TYPES[t] === type) {
+      return true;
+    }
+  }
+  return false;
+};
+
+export class MarkdownNode extends BaseNode {
+  constructor(value, type, id = '') {
+    super(value, id);
+
+    // Type validation.
+    if (typeof type === 'undefined') {
+      throw 'type is required';
+    } else if (!isValidMarkdownType(type)) {
+      throw 'invalid markdown type provided';
+    } else {
+      this.type = type;
+    }
+  }
+}
+
+// @TODO figure out what type of components we need to support.
+export const COMPONENT_TYPES = {
+  jumpLink: {},
+};
+
+const isValidComponentType = (type) => {
+  for (const t in COMPONENT_TYPES) {
+    if (COMPONENT_TYPES[t] === type) {
+      return true;
+    }
+  }
+  return false;
+};
+
+export class ComponentNode extends BaseNode {
+  constructor(value, type, id = '') {
+    super(value, id);
+
+    // Type validation.
+    if (typeof type === 'undefined') {
+      throw 'type is required';
+    } else if (!isValidComponentType(type)) {
+      throw 'invalid component type provided';
+    } else {
+      this.type = type;
+    }
+  }
+}
---
+++ packages/netlify-cms-widget-mdx/test/Node.test.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+import { BaseNode, MarkdownNode, ComponentNode, MARKDOWN_TYPES, COMPONENT_TYPES } from '../src/utils';
+
+describe('BaseNode Class', () => {
+
+  describe('when the constructor is called', () => {
+
+    it('should create a UUID if none is provided', () => {
+      const node = new BaseNode('some value');
+      expect(node.id.length).toEqual(36);
+    });
+
+    it('should return a new node with the provided UUID set correctly', () => {
+      const id = 'b547adb0-cdf8-4348-9221-d14ae8dceb38';
+      const node = new BaseNode('some value', id);
+      expect(node.id).toEqual(id);
+    });
+
+    it('should create a new UUID if the provided id is not valid', () => {
+      const node = new BaseNode('some value', 'not a uuid');
+      expect(node.id.length).toEqual(36);
+    });
+
+    it('should return a node with the provided value set correctly', () => {
+      const value = 'initial value';
+      const node = new BaseNode(value);
+      expect(node.value).toEqual(value);
+    });
+
+    it('should throw an error if missing required arguments', () => {
+      expect(() => {
+        const node = new BaseNode();
+      }).toThrow();
+    });
+  });
+});
+
+describe('MarkdownNode Class', () => {
+
+  describe('when the constructor is called', () => {
+
+    it('should return a node the provided type set correctly', () => {
+      const node = new MarkdownNode('initial value', MARKDOWN_TYPES.text);
+      expect(node.type).toEqual(MARKDOWN_TYPES.text);
+    });
+
+    it('should throw an error if an invalid type is provided', () => {
+      expect(() => {
+        const node = new MarkdownNode('', 'derp');
+      }).toThrow();
+    });
+
+    it('should throw an error if a type is not provided', () => {
+      expect(() => {
+        const node = new MarkdownNode('');
+      }).toThrow();
+    });
+  });
+});
+
+describe('ComponentNode Class', () => {
+
+  describe('when the constructor is called', () => {
+
+    it('should return a node the provided type set correctly', () => {
+      const node = new ComponentNode('initial value', COMPONENT_TYPES.jumpLink);
+      expect(node.type).toEqual(COMPONENT_TYPES.jumpLink);
+    });
+
+    it('should throw an error if an invalid type is provided', () => {
+      expect(() => {
+        const node = new ComponentNode('', 'derp');
+      }).toThrow();
+    });
+
+    it('should throw an error if a type is not provided', () => {
+      expect(() => {
+        const node = new ComponentNode('');
+      }).toThrow();
+    });
+  });
+});
